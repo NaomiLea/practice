a3bfbc07ccb99817b7a6ac46bcf52a23acd3bb45
Naomi Kudren <naomi@missionlabs.co.uk>
2017-04-21 12:13:06 +0100

    adding working ejs partials

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# practice
+express-partials-example
+========================
+
+Rocky, here's a server working.
+
+`git clone git@github.com:sethkrasnianski/express-partials-example.git`
+
+`npm install`
--- server.js
+++ server.js
@@ -1,10 +1,50 @@
-var express = require('express')
-var app = express()
-app.set('view engine', 'ejs');
-app.get('/', function(req, res) {
-    res.render('pages/index');
+
+/**
+ * Module dependencies.
+ */
+
+var express  = require('express'),
+    partials = require('express-partials'),
+    http     = require('http'),
+    path     = require('path');
+
+var app = express();
+
+// all environments
+app.configure(function() {
+  app.set('port', process.env.PORT || 3838);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+
+  // Middleware
+  app.use(partials());
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Routes
+
+// Setup like so
+// app.HTTPMETHOD('path', serverCallBackFunction)
+
+app.get('/', function(res, res) {
+  res.render('index');
+});
+
+app.get('/contact', function(res, res) {
+  res.render('contact');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Application listening on port ' + app.get('port'));
+});
